157c2434b13f42c7cf1b724c7dc52395c8b3d819
AbdiazizH <a.aziz69890@gmail.com>
2021-06-01 21:30:26 +0300

    add changes to run.py file

--- credentials_test.py
+++ credentials_test.py
@@ -1,100 +1,100 @@
-# import unittest # Importing the unittest 
-# from credentials import Credentials #importing credential class
+import unittest # Importing the unittest 
+from credentials import Credentials #importing credential class
 
 
-# class TestCredentials(unittest.TestCase):
-#     '''
-#     Test class that defines test cases for the Credentials class behaviours.
-#     '''
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines test cases for the Credentials class behaviours.
+    '''
     
-#     def setUp(self):
-#         '''
-#         Set up method to run before each test cases.
-#         '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
         
-#         self.new_credential = Credentials('Abdiaziz','Twitter','1234')
+        self.new_credential = Credentials('Abdiaziz','Twitter','1234')
         
-#     def test_init(self):
-#         '''
-#         test_init test case to test if the object is initialized properly
-#         '''
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
 
-#         self.assertEqual(self.new_credential.user_name,"Abdiaziz")
-#         self.assertEqual(self.new_credential.account_name,"Twitter")
-#         self.assertEqual(self.new_credential.password,"1234")
+        self.assertEqual(self.new_credential.user_name,"Abdiaziz")
+        self.assertEqual(self.new_credential.account_name,"Twitter")
+        self.assertEqual(self.new_credential.password,"1234")
     
-#     def test_save_credentials(self):
-#         '''
-#         Test to check if the new users info is saved into the user list
-#         '''
+    def test_save_credentials(self):
+        '''
+        Test to check if the new users info is saved into the user list
+        '''
         
-#         self.new_credential.save_credentials()
-#         self.assertEqual(len(Credentials.credential_list),7)
+        self.new_credential.save_credentials()
+        self.assertEqual(len(Credentials.credential_list),7)
     
-#     def test_delete_credentials(self):
-#         '''
-#         test_delete_credential to test if we can remove a credential from our credential list
-#         '''
-#         self.new_credential.save_credentials()
-#         test_credential = Credentials("Abdiaziz","Twitter","1234") # new credential
-#         test_credential.save_credentials()
+    def test_delete_credentials(self):
+        '''
+        test_delete_credential to test if we can remove a credential from our credential list
+        '''
+        self.new_credential.save_credentials()
+        test_credential = Credentials("Abdiaziz","Twitter","1234") # new credential
+        test_credential.save_credentials()
 
-#         self.new_credential.delete_credentials()# Deleting a credential object
-#         self.assertEqual(len(Credentials.credential_list),4)
+        self.new_credential.delete_credentials()# Deleting a credential object
+        self.assertEqual(len(Credentials.credential_list),4)
     
-#     def test_save_multiple_credential(self):
-#             '''
-#             test_save_multiple_credential to check if we can save multiple credentials
-#             objects to our credential_list
-#             '''
-#             self.new_credential.save_credentials()
-#             test_credential = Credentials("Abdiaziz","Twitter","1234") # new credential
-#             test_credential.save_credentials()
-#             self.assertEqual(len(Credentials.credential_list),9)
+    def test_save_multiple_credential(self):
+            '''
+            test_save_multiple_credential to check if we can save multiple credentials
+            objects to our credential_list
+            '''
+            self.new_credential.save_credentials()
+            test_credential = Credentials("Abdiaziz","Twitter","1234") # new credential
+            test_credential.save_credentials()
+            self.assertEqual(len(Credentials.credential_list),9)
 
-#     def test_display_all_credentials(self):
-#         '''
-#         method that returns a list of all credentials saved
-#         '''
+    def test_display_all_credentials(self):
+        '''
+        method that returns a list of all credentials saved
+        '''
 
-#         self.assertEqual(Credentials.display_credentials(),Credentials.credential_list)
+        self.assertEqual(Credentials.display_credentials(),Credentials.credential_list)
         
-#     def test_find_credential_by_account_name(self):
-#         '''
-#         test to check if we can find a credential by account_name and display information
-#         '''
+    def test_find_credential_by_account_name(self):
+        '''
+        test to check if we can find a credential by account_name and display information
+        '''
 
-#         self.new_credential.save_credentials()
-#         test_credential = Credentials("Abdiaziz","Twitter","1234") # new credential
-#         test_credential.save_credentials()
+        self.new_credential.save_credentials()
+        test_credential = Credentials("Abdiaziz","Twitter","1234") # new credential
+        test_credential.save_credentials()
 
-#         found_credential = Credentials.find_by_account_name("Twitter")
+        found_credential = Credentials.find_by_account_name("Twitter")
 
-#         self.assertEqual(found_credential.account_name,test_credential.account_name)
+        self.assertEqual(found_credential.account_name,test_credential.account_name)
             
             
-#     def test_credential_exists(self):
-#             '''
-#             test to check if we can return a Boolean  if we cannot find the credential.
-#             '''
+    def test_credential_exists(self):
+            '''
+            test to check if we can return a Boolean  if we cannot find the credential.
+            '''
 
-#             self.new_credential.save_credentials()
-#             test_credential = Credentials("Abdiaziz","Twitter","1234") # new credential
-#             test_credential.save_credentials()
+            self.new_credential.save_credentials()
+            test_credential = Credentials("Abdiaziz","Twitter","1234") # new credential
+            test_credential.save_credentials()
 
-#             credential_exists = Credentials.credential_exist("Twitter")
+            credential_exists = Credentials.credential_exist("Twitter")
 
-#             self.assertTrue(credential_exists)
+            self.assertTrue(credential_exists)
     
-#     def test_copy_account_name(self):
-#         '''
-#         Test to confirm that we are copying the account_name from a found credential
-#         '''
+    def test_copy_account_name(self):
+        '''
+        Test to confirm that we are copying the account_name from a found credential
+        '''
 
-#         self.new_credential.save_credentials()
-#         Credentials.find_by_account_name("Twitter")
+        self.new_credential.save_credentials()
+        Credentials.find_by_account_name("Twitter")
         
         
         
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
